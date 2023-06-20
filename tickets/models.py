2abe44334094c6e1766a80801f1b2207d87ba6b5
Brandon Phillips <btp@brandont.dev>
2023-06-20 07:24:42 -0600

    Working on tickets model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+
+class Ticket(models.Model):
+    class TicketStatus(models.TextChoices):
+        OPEN = "Open"
+        IN_PROGRESS = "In progress"
+        WAITING_FOR_SUPPORT = "Waiting for support"
+        WAITING_FOR_CUSTOMER = "Waiting for customer"
+
+    summary_text = models.CharField(max_length=200)
+    reported_date = models.DateTimeField("Reported")
+    status = models.CharField(
+        max_length=30,
+        choices=TicketStatus.choices,
+        default=TicketStatus.OPEN,
+    )
+
+    def __str__(self):
+        return self.summary_text
+
+    def is_new(self):
+        return self.reported_date >= timezone.now() - datetime.timedelta(hours=1)
+
+    def get_status(self):
+        return self.status
+
+    def change_status(self, status):
+        self.status = status
